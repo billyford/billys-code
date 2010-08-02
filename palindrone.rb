a994c0ae0ae442457b53b5ff8af7dbe775c0001e
billyford <maninchatroom@hotmail.com>
2010-08-02 18:55:08 +0100

    now does 5

--- palindrone.rb
+++ palindrone.rb
@@ -1,11 +1,17 @@
 puts "type word here"
 word = gets.chomp
 
-#puts word[0,1]
-word_length = word.length
+#puts word[1,1]
 #puts word_length.to_s
-#puts word[word_length-1 ,word_length]
+#puts word[word_length-2 ,1]
+
+word_length = word.length
 if (word[0,1] == word[word_length-1 ,word_length])
+	puts "ok"
+else
+	puts "no"
+end 
+if (word[1,1] == word[word_length-2 ,1])
 	puts "ok"
 else
 	puts "no"
